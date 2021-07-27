c533de9266cf43cd7203b4b70eddd5237fd3dcf6
Nicolas Poltorak <nicolaspoltorak@outlook.fr>
2021-07-27 13:52:07 +0200

    Contenu produitsPage (manque btn et personnalisation)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let product = [];
+let productHomePage = [];
 const cardContainer = document.getElementsByClassName("card-container");
-const fetchProduct = async () => {
+const fetchProductHome = async () => {
   await fetch("http://localhost:3000/api/cameras")
     .then((res) => res.json())
-    .then((data) => (product = data));
-  cardContainer[0].innerHTML += product
+    .then((data) => (productHomePage = data));
+  cardContainer[0].innerHTML += productHomePage
     .map(
       (item) =>
         `
@@ -23,13 +23,37 @@ const fetchProduct = async () => {
     .join(" ");
 };
 
-fetchProduct();
+fetchProductHome();
 
-//-------- Ajout d'une div
+let productProductPage = [];
+const productCardContainer = document.getElementsByClassName("product-cards");
+const fetchProductPage = async () => {
+  await fetch("http://localhost:3000/api/cameras")
+    .then((res) => res.json())
+    .then((data) => (productProductPage = data));
+  productCardContainer[0].innerHTML += productProductPage
+    .map(
+      (item) =>
+        `
+    <div class="row">
+      <div class="col-12 col-sm-12	col-md-12	col-lg-12	col-xl-12">
+        <h1> ${item.name} </h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-4	col-sm-4	col-md-8	col-lg-8	col-xl-8">
+        <figure class="figure">
+          <img class="figure-img img-fluid img-cards" src=${item.imageUrl} alt="Photo de ${item.name}" />
+        </figure>
+      </div>
+      <div class="col-8	col-sm-8	col-md-4	col-lg-4	col-xl-4">
+        <p> ${item.description} </p>
+        <p> ${item.price} </p>
+      </div>
+    </div>
+     `
+    )
+    .join(" ");
+};
 
-// let newDiv = document.createElement("div");
-// let newContent = document.createTextNode("Try me");
-// const noob = document.getElementsByClassName("card-container");
-// console.log(noob);
-// newDiv.appendChild(newContent);
-// noob[0].appendChild(newDiv);
+fetchProductPage();
