9806235588c4bd843a4578a021379e8b9dd6765d
Nicolas Poltorak <nicolaspoltorak@outlook.fr>
2021-07-27 15:55:25 +0200

    Affichage lenses (modif à faire pour afficher un seul produit par ID)

--- index.js
+++ index.js
@@ -31,29 +31,41 @@ const fetchProductPage = async () => {
   await fetch("http://localhost:3000/api/cameras")
     .then((res) => res.json())
     .then((data) => (productProductPage = data));
-  productCardContainer[0].innerHTML += productProductPage
-    .map(
-      (item) =>
-        `
-    <div class="row">
-      <div class="col-12 col-sm-12	col-md-12	col-lg-12	col-xl-12">
-        <h1> ${item.name} </h1>
+  productCardContainer[0].innerHTML += productProductPage.map((item) => {
+    let lenses = "";
+    if (item.lenses.length >= 2) {
+      lenses += `<select class="lense">
+      <option value=""> Personnaliez votre lense</option> `;
+      for (i = 0; i < item.lenses.length; i++) {
+        if (item.lenses[i]) {
+          lenses += `<option value=" ${item.lenses[i]}"> ${item.lenses[i]} </option>`;
+        }
+      }
+      lenses += `</select>`;
+    } else if (item.lenses.length == 1) {
+      lenses = `<p> ${item.lenses[0]} </p>
+      `;
+    }
+    return `
+      <div class="row">
+        <div class="col-12 col-sm-12	col-md-12	col-lg-12	col-xl-12">
+          <h1> ${item.name} </h1>
+          </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-4	col-sm-4	col-md-8	col-lg-8	col-xl-8">
-        <figure class="figure">
-          <img class="figure-img img-fluid img-cards" src=${item.imageUrl} alt="Photo de ${item.name}" />
-        </figure>
+      <div class="row">
+        <div class="col-4	col-sm-4	col-md-8	col-lg-8	col-xl-8">
+          <figure class="figure">
+            <img class="figure-img img-fluid img-cards" src=${item.imageUrl} alt="Photo de ${item.name}" />
+          </figure>
+        </div>
+        <div class="col-8	col-sm-8	col-md-4	col-lg-4	col-xl-4">
+          <p> ${item.description} </p>
+          <p> ${item.price} </p>
+            ${lenses};
+        </div>
       </div>
-      <div class="col-8	col-sm-8	col-md-4	col-lg-4	col-xl-4">
-        <p> ${item.description} </p>
-        <p> ${item.price} </p>
-      </div>
-    </div>
-     `
-    )
-    .join(" ");
+      `;
+  });
 };
 
 fetchProductPage();
