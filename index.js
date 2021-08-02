02eeeaeb8cfd26a926fa4a0a799ce13fcea925b5
Nicolas Poltorak <nicolaspoltorak@outlook.fr>
2021-08-02 11:03:14 +0200

    Page produit (produit unique)

--- index.js
+++ index.js
@@ -15,57 +15,61 @@ const fetchProductHome = async () => {
       <button class="btn btn-price" type="button">Ajouter au panier</button>
       </div>
       <figure class="figure">
-      <img class="figure-img img-fluid img-cards" src=${item.imageUrl} alt="Photo de ${item.name}" />
+      <img class="figure-img img-fluid img-cards" id="${item._id}" src=${item.imageUrl} alt="Photo de ${item.name}" />
       </figure>
     </div>
      `
     )
     .join(" ");
+  document.querySelectorAll(".img-cards").forEach((item) => {
+    console.log("texte");
+    item.addEventListener("click", (e) => {
+      // getProduct(e.target.id);
+      window.location.assign("./assets/produits.html?id=" + e.target.id);
+    });
+  });
 };
 
 fetchProductHome();
 
-let productProductPage = [];
-const productCardContainer = document.getElementsByClassName("product-cards");
-const fetchProductPage = async () => {
-  await fetch("http://localhost:3000/api/cameras")
-    .then((res) => res.json())
-    .then((data) => (productProductPage = data));
-  productCardContainer[0].innerHTML += productProductPage.map((item) => {
-    let lenses = "";
-    if (item.lenses.length >= 2) {
-      lenses += `<select class="lense">
-      <option value=""> Personnaliez votre lense</option> `;
-      for (i = 0; i < item.lenses.length; i++) {
-        if (item.lenses[i]) {
-          lenses += `<option value=" ${item.lenses[i]}"> ${item.lenses[i]} </option>`;
-        }
-      }
-      lenses += `</select>`;
-    } else if (item.lenses.length == 1) {
-      lenses = `<p> ${item.lenses[0]} </p>
-      `;
-    }
-    return `
-      <div class="row">
-        <div class="col-12 col-sm-12	col-md-12	col-lg-12	col-xl-12">
-          <h1> ${item.name} </h1>
-          </div>
-      </div>
-      <div class="row">
-        <div class="col-4	col-sm-4	col-md-8	col-lg-8	col-xl-8">
-          <figure class="figure">
-            <img class="figure-img img-fluid img-cards" src=${item.imageUrl} alt="Photo de ${item.name}" />
-          </figure>
-        </div>
-        <div class="col-8	col-sm-8	col-md-4	col-lg-4	col-xl-4">
-          <p> ${item.description} </p>
-          <p> ${item.price} </p>
-            ${lenses};
-        </div>
-      </div>
-      `;
-  });
+/* ------------------------------------ Formulaire ----------------------------- */
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll('input[type="text"]');
+let firstname, lastname, adress, city, email;
+
+const error = (tag, message, valid) => {
+  const container = document.querySelector("." + tag + "-container");
+  const span = document.querySelector("." + tag + "-container > span");
+
+  if (!valid) {
+    container.classList.add("error");
+    span.textContent = message;
+  }
 };
 
-fetchProductPage();
+const nameChecker = (value) => {
+  if (value.length < 0) {
+    firstname = null;
+  } else if (!value.match(/^[a-zA-Z]*$/)) {
+    errorDisplay(
+      "firstname",
+      "Votre prénom ne peux pas contenir de caractère spéciaux ou de chiffres"
+    );
+    firstname = null;
+  } else {
+    errorDisplay("firstname", "", true);
+    pseudo = value;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "firstname":
+        nameChecker(e.target.value);
+        break;
+      default:
+        nul;
+    }
+  });
+});
---
+++ js/pageProduits.js
@@ -0,0 +1,47 @@
+let adress = window.location.search;
+let productId = adress.replace("?id=", "");
+
+let productPage = [];
+const productCardContainer = document.getElementsByClassName("product-cards");
+const getProduct = async (id) => {
+  await fetch("http://localhost:3000/api/cameras/" + id)
+    .then((res) => res.json())
+    .then((data) => (productPage = data));
+
+  let lenses = "";
+  if (productPage.lenses.length >= 2) {
+    lenses += `<select class="lense">
+      <option value=""> Personnaliez votre lense</option> `;
+    for (i = 0; i < productPage.lenses.length; i++) {
+      if (productPage.lenses[i]) {
+        lenses += `<option value=" ${productPage.lenses[i]}"> ${productPage.lenses[i]} </option>`;
+      }
+    }
+    lenses += `</select>`;
+  } else if (productPage.lenses.length == 1) {
+    lenses = `<p> ${productPage.lenses[0]} </p>
+      `;
+  }
+
+  productCardContainer[0].innerHTML = `
+      <div class="row">
+        <div class="col-12 col-sm-12	col-md-12	col-lg-12	col-xl-12">
+          <h1> ${productPage.name} </h1>
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-4	col-sm-4	col-md-8	col-lg-8	col-xl-8">
+          <figure class="figure">
+            <img class="figure-img img-fluid img-cards" src=${productPage.imageUrl} alt="Photo de ${productPage.name}" />
+          </figure>
+        </div>
+        <div class="col-8	col-sm-8	col-md-4	col-lg-4	col-xl-4">
+          <p> ${productPage.description} </p>
+          <p> ${productPage.price} </p>
+            ${lenses};
+        </div>
+      </div>
+      `;
+};
+
+getProduct(productId);
